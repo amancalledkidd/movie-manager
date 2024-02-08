270fd8656f8ea68ca36c3006dbe04dfae03d98b6
Kumani K <kumanikidd@gmail.com>
2024-02-08 16:02:44 +0000

    completed Film model and tests

---
+++ api/src/main/java/com/moviemanager/api/model/Film.java
@@ -0,0 +1,60 @@
+package com.moviemanager.api.model;
+
+public class Film {
+    private String title;
+    private String director;
+    private int releaseYear;
+    private String genre;
+
+    public Film(String title, String director, int releaseYear, String genre) {
+        this.title = title;
+        this.director = director;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "title='" + title + '\'' +
+                ", director='" + director + '\'' +
+                ", releaseYear=" + releaseYear +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
+}
+
+
---
+++ api/src/test/java/com/moviemanager/api/FilmTest.java
@@ -0,0 +1,67 @@
+package com.moviemanager.api;
+
+import com.moviemanager.api.model.Film;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmTest {
+
+    private Film film;
+
+    @BeforeEach
+    void setUp() {
+        film = new Film("Inception", "Christopher Nolan", 2010, "Science Fiction");
+    }
+
+    @Test
+    void getTitle() {
+        assertEquals("Inception", film.getTitle());
+    }
+
+    @Test
+    void setTitle() {
+        film.setTitle("Interstellar");
+        assertEquals("Interstellar", film.getTitle());
+    }
+
+    @Test
+    void getDirector() {
+        assertEquals("Christopher Nolan", film.getDirector());
+    }
+
+    @Test
+    void setDirector() {
+        film.setDirector("Steven Spielberg");
+        assertEquals("Steven Spielberg", film.getDirector());
+    }
+
+    @Test
+    void getReleaseYear() {
+        assertEquals(2010, film.getReleaseYear());
+    }
+
+    @Test
+    void setReleaseYear() {
+        film.setReleaseYear(2014);
+        assertEquals(2014, film.getReleaseYear());
+    }
+
+    @Test
+    void getGenre() {
+        assertEquals("Science Fiction", film.getGenre());
+    }
+
+    @Test
+    void setGenre() {
+        film.setGenre("Action");
+        assertEquals("Action", film.getGenre());
+    }
+
+    @Test
+    void testToString() {
+        String expected = "Film{title='Inception', director='Christopher Nolan', releaseYear=2010, genre='Science Fiction'}";
+        assertEquals(expected, film.toString());
+    }
+}
