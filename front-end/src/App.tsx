1eb576c7cb3df894af6ce69d28fe88ccbc6f1db4
Kumani K <kumanikidd@gmail.com>
2024-02-13 15:58:29 +0000

    routes added to app

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,22 @@
-import './App.scss'
-import Home from './components/Home/Home'
+import './App.scss';
+import FilmInfoPage from './components/FilmInfoPage/FilmInfoPage';
+import Home from './components/Home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserFilmPage from './components/UserFilmPage/UserFilmPage';
+
 
 function App() {
-
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/film/:id" element={<FilmInfoPage/>} />
+          <Route path="/user-films" element={<UserFilmPage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
