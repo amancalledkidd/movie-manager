68cf8af646b61cbbcd9a2b618b0e2984fc11ca2f
Kumani K <kumanikidd@gmail.com>
2024-02-13 15:58:47 +0000

    updated film item

--- front-end/src/components/FilmItem/FilmItem.tsx
+++ front-end/src/components/FilmItem/FilmItem.tsx
@@ -1,16 +1,23 @@
 import './FilmItem.scss';
 import { Film } from '../../types/Film'
+import { Link } from 'react-router-dom';
 
 type FilmItemProps = {
     film: Film;
 }
 
 const FilmItem = ({ film }: FilmItemProps) => {
-  return (
-    <div className="film-item">
-      <h2>{film.title}</h2>
-    </div>
-  );
+    const imageUrl = film.posterPath
+        ? `https://image.tmdb.org/t/p/w500${film.posterPath}.jpg`
+        : '/path-to-default-image.jpg';
+
+    return (
+        <div className="film-item-container">
+            <Link to={`/film/${film.apiId}`}>
+                <img src={imageUrl} alt={film.title} className="film-item-container__film-image" />
+            </Link>
+        </div>
+      );
 }
 
 export default FilmItem;
