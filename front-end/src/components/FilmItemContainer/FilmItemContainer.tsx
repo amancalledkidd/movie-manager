183c57e8e4f6cd82a9cd00e9a0a86f0002b93114
Kumani K <kumanikidd@gmail.com>
2024-02-14 15:51:55 +0000

    Container styling update

--- front-end/src/components/FilmItemContainer/FilmItemContainer.tsx
+++ front-end/src/components/FilmItemContainer/FilmItemContainer.tsx
@@ -4,15 +4,16 @@ import { Film } from '../../types/Film'
 
 type FilmItemContainerProps = {
     films: Film[]
+    title: string
 }
 
-const FilmItemContainer = ({films}: FilmItemContainerProps) => {
+const FilmItemContainer = ({films, title}: FilmItemContainerProps) => {
     return (
-        <div>
-        <h1>FilmItemContainer</h1>
-        <div>
-            {films.map(film => <FilmItem film={film} key={film.id} />)}
-        </div>
+        <div className='film-container'>
+            <h1 className='film-container__title'>{title}</h1>
+            <div className='film-container__items'>
+                {films.map(film => <FilmItem film={film} key={film.id} />)}
+            </div>
         </div>
     );
 };
