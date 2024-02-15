96035bc992007835257606b2d032a42af7789f6d
Kumani K <kumanikidd@gmail.com>
2024-02-15 15:25:02 +0000

    delete button added to reviews

--- front-end/src/components/ReviewItem/ReviewItem.tsx
+++ front-end/src/components/ReviewItem/ReviewItem.tsx
@@ -1,16 +1,19 @@
 import './ReviewItem.scss'
 import { Review } from '../../types/Review'
+import Delete from '../../assets/icons/delete.svg'
 
 type ReviewItemProps = {
     review: Review
+    onclick: () => void;
 }
 
-const ReviewItem = ({review}: ReviewItemProps) => {
+const ReviewItem = ({review, onclick}: ReviewItemProps) => {
     return (
         <div className='review review--background'>
             <h3 className='review__reviewer'>{review.reviewerName}</h3>
             <p className='review__text'>{review.reviewText}</p>
             <p className='review__rating'>{review.rating}</p>
+            <img src={Delete} alt='delete' className='review__delete' onClick={onclick}/>
         </div>
     );
 };
