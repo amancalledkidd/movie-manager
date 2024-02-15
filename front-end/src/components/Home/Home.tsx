28a7e1d351c8f5ea1d6f003196ca4af6d9b0e0da
Kumani K <kumanikidd@gmail.com>
2024-02-15 15:24:18 +0000

    added more film containers and applied some styling

--- front-end/src/components/Home/Home.tsx
+++ front-end/src/components/Home/Home.tsx
@@ -7,15 +7,18 @@ import Navbar from '../Navbar/Navbar';
 const Home = () => {
 
     const [popularMovies, setPopularMovies] = useState<FilmInfo[]>([]);
+    const [upcomingMovies, setUpcomingMovies] = useState<FilmInfo[]>([]);
+    const [topRatedMovies, setTopRatedMovies] = useState<FilmInfo[]>([]);
 
     useEffect(() => {
-        fetchPopularMovies();
+        fetchMovies("popular");
+        fetchMovies("upcoming");
+        fetchMovies('top_rated');
     }, []);
 
-
-    const fetchPopularMovies = async () => {
+    const fetchMovies = async (param: string) => {
         const apiKey = import.meta.env.VITE_TMDB_KEY;
-        const url = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${apiKey}`;
+        const url = `https://api.themoviedb.org/3/movie/${param}?language=en-US&page=1&api_key=${apiKey}`;
         const options = {
             method: 'GET',
             headers: {
@@ -28,7 +31,13 @@ const Home = () => {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
             const json = await response.json();
-            setPopularMovies(json.results);
+            if (param === "upcoming") {
+                setUpcomingMovies(json.results);
+            } else if (param === "popular") {
+                setPopularMovies(json.results);
+            } else if (param === "top_rated") {
+                setTopRatedMovies(json.results);
+            }
             console.log(json);
         } catch (err) {
             console.error('error:', err);
@@ -37,10 +46,19 @@ const Home = () => {
 
     return (
         <div>
-        <Navbar />
-        <PopularFilmContainer heading="Trending Films" popularMovies={popularMovies} />
+            <Navbar />
+            <section className="home">
+                <div className="home__heading">
+                    <h1 className="home__title">Movie Manager</h1>
+                </div>
+                <div className="home__containers">
+                    <PopularFilmContainer heading="Top Rated" popularMovies={topRatedMovies} />
+                    <PopularFilmContainer heading="Trending Films" popularMovies={popularMovies} />
+                    <PopularFilmContainer heading="Upcoming Films" popularMovies={upcomingMovies} />
+                </div>
+            </section>
         </div>
     );
-};
+}
 
 export default Home;
