5d7428b3f7492cd0bba09495bcc4c6777f9fffa2
Kumani K <kumanikidd@gmail.com>
2024-02-15 11:42:31 +0000

    styling for review container

--- front-end/src/components/ReviewContainer/ReviewContainer.tsx
+++ front-end/src/components/ReviewContainer/ReviewContainer.tsx
@@ -1,21 +1,41 @@
 import './ReviewContainer.scss'
 import { Review } from '../../types/Review'
 import ReviewItem from '../ReviewItem/ReviewItem';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type ReviewContainerProps = {
-    reviews: Review[]
+    filmId: number;
 }
 
-const ReviewContainer = ({reviews}: ReviewContainerProps) => {
+const ReviewContainer = ({filmId}: ReviewContainerProps) => {
     const [showAll, setShowAll] = useState(false);
+    const [reviews, setReviews] = useState<Review[]>([]);
     const reviewsToShow = showAll ? reviews : reviews.slice(0, 3);
+
+    useEffect(() => {
+        fetchReviews(filmId);
+    }, [filmId]);
+
+
+    const fetchReviews = async (filmId: number) => {
+        try {
+            const response = await fetch(`http://localhost:8080/film/${filmId}/reviews`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const json = await response.json();
+            setReviews(json);
+        } catch (err) {
+            console.error('error:', err);
+        }
+    }
+
 
     return (
         <div className='review-container'>
             <h2 className='review-container__title'>Reviews</h2>
             <div className='review-container__items'>
-                {reviewsToShow.map(review => <ReviewItem review={review} key={review.id} />)}
+                {reviews.map(review => <ReviewItem review={review} key={review.id} />)}
             </div>
             {reviews.length > 3 && <button onClick={() => setShowAll(!showAll)} className='review-container__show-all'>{showAll ? 'Show Less' : 'Show All'}</button>}
         </div>
